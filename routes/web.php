cf21cbe09462a12f7a8aae53000e747244196668
Hilmi Razib Yusuf <hilmiyusuf197@gmail.com>
2025-07-15 16:20:33 +0700

    feat(routes): add authentication and attendance routes

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthController::class, 'index'])->name('login')->middleware('guest');
+Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::post('/login/authenticate', [AuthController::class, 'authenticate'])->name('login.authenticate');
+Route::post('/attendance/checkin/face', [AuthController::class, 'checkinFace'])->name('attendance.checkin.face');
+Route::post('/attendance/checkout/face', [AuthController::class, 'checkoutFace'])->name('attendance.checkout.face');
+Route::post('/attendance/checkin/qr', [AuthController::class, 'checkinQr'])->name('attendance.checkin.qr');
+Route::post('/attendance/checkout/qr', [AuthController::class, 'checkoutQr'])->name('attendance.checkout.qr');
