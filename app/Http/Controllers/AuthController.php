91dfd86deda3fd17267520a641d4676e628fbf05
Hilmi Razib Yusuf <hilmiyusuf197@gmail.com>
2025-07-15 16:16:28 +0700

    refactor(auth): refine authentication logic and add attendance view methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -10,6 +13,75 @@ class AuthController extends Controller
     {
         return view('auth.login',[
             "title" => "Log In"
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        date_default_timezone_set('Asia/Jakarta');
+
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+
+        if ($user) {
+            if ($user->masa_berlaku && $user->masa_berlaku <= date('Y-m-d')) {
+                Alert::error('Failed', 'Username / Password Salah / Akun Tidak Aktif');
+                return back();
+            } else {
+                if (Auth::attempt($credentials)) {
+                    if ($user->hasRole('admin')) {
+                        $user->update([
+                            'is_admin' => 'admin'
+                        ]);
+                    } else {
+                        $user->update([
+                            'is_admin' => 'user'
+                        ]);
+                    }
+
+                    $request->session()->regenerate();
+                    return redirect()->intended('/dashboard');
+                } else {
+                    Alert::error('Failed', 'Username / Password Salah / Akun Tidak Aktif');
+                    return back();
+                }
+            }
+        } else {
+            Alert::error('Failed', 'Username / Password Salah / Akun Tidak Aktif');
+            return back();
+        }
+
+    }
+
+    public function checkinFace()
+    {
+        return view('auth.checkin_face', [
+            "title" => "Presensi Masuk",
+        ]);
+    }
+
+    public function checkoutFace()
+    {
+        return view('auth.checkout_face', [
+            "title" => "Presensi Pulang",
+        ]);
+    }
+
+    public function checkinQr()
+    {
+        return view('auth.checkin_qr', [
+            "title" => "Presensi QR Code Masuk",
+        ]);
+    }
+
+    public function checkoutQr()
+    {
+        return view('auth.checkout_qr', [
+            "title" => "Presensi QR Code Pulang",
         ]);
     }
 }
