f6b5f783a413cb9c48b51a3f35a34f738076e53e
Hilmi Razib Yusuf <hilmiyusuf197@gmail.com>
2025-07-17 13:43:43 +0700

    feat(auth): Add admin login page and assets
    This commit introduces the admin-specific login page. It includes the necessary route, view, and frontend assets to render the login form.
    
    - Add route `/login-admin` in `web.php` to handle the request.
    - Create `login_admin.blade.php` view for the login form UI.
    - Add required CSS, JS, and image assets for the admin theme.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::get('/login-admin', [authController::class, 'showAdminLoginForm'])->name('login.admin');
 Route::post('/login/authenticate', [AuthController::class, 'authenticate'])->name('login.authenticate');
 Route::post('/attendance/checkin/face', [AuthController::class, 'checkinFace'])->name('attendance.checkin.face');
 Route::post('/attendance/checkout/face', [AuthController::class, 'checkoutFace'])->name('attendance.checkout.face');
